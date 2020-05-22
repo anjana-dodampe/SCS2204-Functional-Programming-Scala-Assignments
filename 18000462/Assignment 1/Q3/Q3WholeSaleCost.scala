f5ca837a77af853b46cd38445ecaa861c5103ab9
anjana-dodampe <ahmdodampe@hotmail.com>
2020-05-22 11:59:18 +0530

    Done

--- 18000462/Assignment 1/Q3/Q3WholeSaleCost.scala
+++ 18000462/Assignment 1/Q3/Q3WholeSaleCost.scala
@@ -7,7 +7,7 @@ object wholeSaleCost extends App{
   def discount(realPrice:Double):Double = realPrice*0.4;
 
   //calculate the Shipping Cost
-  def shipCost(x:Int):Double=3*x+(x-50)*0.75;
+  def shipCost(x:Int):Double=if(x>50) 3*50+(x-50)*0.75 else x*3;
 
   //Calculates the wholesale cost
   val wholesalecost = priceOfBooks(60) - discount(priceOfBooks(60)) + shipCost(60);
